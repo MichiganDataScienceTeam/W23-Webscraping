8c524c70901e86aa70ece783689c88b67c160f3a
Justin Paul <justpaul@umich.edu>
2023-01-14 11:37:22 -0500

    small refactor

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,10 +2,11 @@ import os
 import random
 
 from flask import Flask, render_template
-from ascii_art import read_in_art
+from scripts.ascii_art import read_in_art
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -14,24 +15,27 @@ def index():
 
 @app.route('/wordsearch/')
 def wordsearch():
-    wordrow = [chr(ord('a')+ x) * 20 for x in range(5)]
+    wordrow = [chr(ord('a') + x) * 20 for x in range(5)]
     return render_template('ctf.html', wordrow=wordrow)
+
 
 @app.route('/caesar/')
 def caesar_scrape():
     with open('static/caesar.txt', 'r') as f:
         lines = f.readlines()
-        
-    return render_template('caesar.html',word_caesar=zip(lines[:len(lines)//2], lines[len(lines)//2:]))
+
+    return render_template('caesar.html', word_caesar=zip(lines[:len(lines)//2], lines[len(lines)//2:]))
+
 
 @app.route('/art/')
 def art_index():
-    messages=[
+    messages = [
         'url: /art/<art>/',
         '<art> can be "elephant", "dogs", "beaver", or "tweetie"'
-        ]
-    
+    ]
+
     return render_template('not_found.html', messages=messages)
+
 
 @app.route('/shuffled_art/<art>/')
 def unshuffled_ascii_art(art):
@@ -41,12 +45,13 @@ def unshuffled_ascii_art(art):
     random.shuffle(ind_lines)
     return render_template('ascii_shuffled.html', art=art, lines=ind_lines)
 
+
 @app.route('/art/<art>/')
 def ascii_art(art):
     lines = read_in_art(f'static/{art}.txt', replace=False)
 
-    # random.shuffle(ind_lines)
     return render_template('ascii_art.html', art=art, line=''.join(lines))
-    
+
+
 if __name__ == '__main__':
     app.run()
