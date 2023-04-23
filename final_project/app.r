a2f0a3373fa602924f5ba200143d29bc3f5f6928
Justin Paul <justpaul@umich.edu>
2023-04-22 23:30:29 -0400

    add teacher evals

--- final_project/app.r
+++ final_project/app.r
@@ -5,7 +5,7 @@ library(dplyr)
 library(DT)
 library(shinyjs)
 
-df <- read_csv('data/dummy_cse.csv', show_col_types=FALSE)
+df <- read_csv('data/cse_final.csv', show_col_types=FALSE)
 #print(df)
 
 
@@ -22,26 +22,27 @@ ui <- fluidPage(
       ## make sure to use checking so that users can't exceed these values
       sliderInput(
         "clarity_range", "Clarity", 
-        value = c(min(df$clarity), max(df$clarity)),
-        min = min(df$clarity), 
-        max = max(df$clarity)
+        value = c(min(df$clarity, na.rm=TRUE), max(df$clarity, na.rm=TRUE)),
+        min = 0, 
+        max = 100
         ),
       
       sliderInput(
         "preparedness_range", "Preparedness", 
-        value = c(min(df$preparedness), max(df$preparedness)),
-        min = min(df$preparedness), 
-        max = max(df$preparedness)
+        value = c(min(df$preparedness, na.rm=TRUE), max(df$preparedness, na.rm=TRUE)),
+        min = 0, 
+        max = 100
         ),
       
       sliderInput(
         "respect_range", "Respect", 
-        value = c(min(df$respect), max(df$respect)),
-        min = min(df$respect), 
-        max = max(df$respect)
+        value = c(min(df$respect, na.rm=TRUE), max(df$respect, na.rm=TRUE)),
+        min = 0, 
+        max = 100
         ),
       
-      checkboxInput("research_check", "Has an Open Research Position?")
+      checkboxInput("research_check", "Has an Open Research Position?"),
+      checkboxInput("taught_check", "Has The Professor Taught/Been Evaluated?")
       
     ),
     
@@ -54,20 +55,26 @@ ui <- fluidPage(
 server <- function(input, output) {
   # Filter data based on range sliders
   filtered_data <- reactive({
-    df %>% 
+
+    if (input$taught_check) {
+      df %>% 
       filter( 
         input$clarity_range[1]<= clarity & clarity <= input$clarity_range[2] &
         input$preparedness_range[1] <= preparedness & preparedness <= input$preparedness_range[2] &
         input$respect_range[1] <= respect & respect <= input$respect_range[2] &
         (!input$research_check | research)
       )
-           
+    }
+    else {
+      df
+    }
+         
   })
   
   # Display filtered data in table format
   output$profs <- renderDataTable({
     filtered_data() %>% 
-      select(name, email, clarity, preparedness, respect, research, interests)
+      select(name, email, clarity, preparedness, respect, research, lab, interests)
   }, options = list(pageLength = 5), server = TRUE)
 
   # Display selected row data in popup when a row is clicked
@@ -79,11 +86,8 @@ server <- function(input, output) {
         renderText(paste0("Email: ", row_data$email)),
         renderText(paste0("Lab: ", row_data$lab)),
         renderText(paste0("Office: ", row_data$office)),
-        renderText(paste0("Clarity: ", row_data$clarity)),
-        renderText(paste0("Preparedness: ", row_data$preparedness)),
-        renderText(paste0("Respect: ", row_data$respect)),
-        renderText(paste0("Research: ", row_data$research)),
-        renderText(paste0("Interests: ", row_data$interests)),
+        renderText(paste0("Students Taught: ", row_data$`students taught`)),
+        renderText(paste0("Terms Taught: ", row_data$`terms taught`)),
         easyClose = TRUE, footer = NULL
       ))
     }
